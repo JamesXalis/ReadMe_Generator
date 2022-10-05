fa56b9edcbf9a84d5a3596bfd6c4c0309142d82a
James Xalis <jamesxalis@icloud.com>
2022-10-05 16:10:58 -0700

    added questions to ask and included packages needed

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'username',
+        message: "what is your Github Username",
+    },
+    {
+        name: 'email',
+        message: "What is your email?",
+    },
+    {
+        name: 'project',
+        message: "what is your project's name?",
+    },
+    {
+        name: 'description',
+        message: "Please write a short discription of your project:",
+    },
+    {
+        name: 'license',
+        message: "What kind of license should your project have?",
+    },
+    {
+        name: 'dependencies',
+        message: "What command should be run to install dependencies?",
+    },
+    {
+        name: 'run',
+        message: "What command should be run to run tests?",
+    },
+    {
+        name: 'use',
+        message: "What does the user need to know about using the repo?",
+    },
+    {
+        name: 'contribute',
+        message: "What does the user need to know about contributing to the repo?",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
