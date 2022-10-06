c70e3ad25b8fa8b099e71696e41dcd7033aa510f
James Xalis <jamesxalis@icloud.com>
2022-10-05 17:14:31 -0700

    added template for the README file

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { listenerCount } = require('process');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,12 +22,15 @@ const questions = [
         message: "Please write a short discription of your project:",
     },
     {
+        type: 'list',
         name: 'license',
         message: "What kind of license should your project have?",
+        choices: ["MIT","Apache","Mozilla","none"],
     },
     {
-        name: 'dependencies',
+        name: 'install',
         message: "What command should be run to install dependencies?",
+        default: "N/A"
     },
     {
         name: 'run',
@@ -43,10 +47,20 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log("success!"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            generateMarkdown(answers);
+        }
+        )
+        .catch (err => err ? console.log ("Try Again"): console.log("Generating README..."))
+}
 
 // Function call to initialize app
 init();
--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +21,33 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  `
+  <h1 align="center">${answers.projectTitle} 👋</h1>
+    
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+  ## Description
+  🔍 ${answers.description}
+  ## Installation
+  💾 ${answers.installation}
+  ## Usage
+  💻 ${answers.usage}
+  ## License
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+  <br />
+  This application is covered by the ${answers.license} license. 
+  ## Contributing
+  👪 ${answers.contributing}
+  ## Tests
+  ✏️ ${answers.tests}
+  ## Questions
+  ✋ ${answers.questions}<br />
+  <br />
+  :octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+  <br />
+  ✉️ Email me with any questions: ${answers.email}<br /><br />
+  _This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 🔥🔥🔥_
+      `;
+    }
 
-`;
-}
 
 module.exports = generateMarkdown;
